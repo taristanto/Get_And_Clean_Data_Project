f4c0f555dc8406ac36d0725ceea917a20babc211
Theo <taristanto@verizon.net>
2014-12-20 19:00:20 -0500

    run_analysis.R
    Add Header comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,17 @@
+##********************************************************************************************
+##Author: T. Aristanto    12/20/2014
+##
+##Coursera.org Getting and Cleaning Data class Project
+##This functiuon will:
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names. 
+## 5. From the data set in step 4, creates a second, independent tidy data set with the 
+##    average of each variable for each activity and each subject.
+##********************************************************************************************
+
+
 run_analysis <- function(){
 	## Load the files in to table
 	ytest <- read.table("UCI_HAR_Dataset/test/Y_test.txt")
